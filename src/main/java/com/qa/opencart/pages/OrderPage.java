a81619e47dedea802511f850b552a4d057ed89f6
Dhrumil Soni <dhrumil.soni@veriday.com>
2022-04-25 15:47:04 +0530

    New Commit

--- src/main/java/com/qa/opencart/pages/OrderPage.java
+++ src/main/java/com/qa/opencart/pages/OrderPage.java
@@ -1,14 +1,17 @@
 package com.qa.opencart.pages;
 
 public class OrderPage {
-  
-  public void getOrderID(){
-    System.out.println("Order id is 12345");
-  }
+
+	public void getOrderID() {
+		System.out.println("Order id is 12345");
+	}
 
 	public void makeOrder() {
 		System.out.println("order page....");
+		System.out.println("Order is generated...");
+
 	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
