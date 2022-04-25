86c2cffc0f3745e19d623a79c90c012649d8c326
Dksoni81291 <37045526+Dksoni81291@users.noreply.github.com>
2022-04-25 13:18:17 +0530

    Create Sample.java

---
+++ src/main/java/com/qa/opencart/pages/Sample.java
@@ -0,0 +1,15 @@
+package com.qa.opencart.pages;
+
+public class Sample {
+	
+	public void login() {
+		String username = "Soni";
+    String password = "Soni@123";
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
