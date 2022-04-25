beef643c2460577cd7693c3047746a4b83114baf
Dksoni81291 <37045526+Dksoni81291@users.noreply.github.com>
2022-04-25 13:44:41 +0530

    Create OrderPage.java

---
+++ src/main/java/com/qa/opencart/pages/OrderPage.java
@@ -0,0 +1,17 @@
+package com.qa.opencart.pages;
+
+public class OrderPage {
+  
+  public void getOrderID(){
+    System.out.println("Order id is 12345");
+  }
+
+	public void makeOrder() {
+		System.out.println("order page....");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
