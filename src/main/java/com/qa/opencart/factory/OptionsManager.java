d75d35e3e8e88d2383342fba18c8b341694945da
Dhrumil Soni <dhrumil.soni@veriday.com>
2022-05-03 16:46:51 +0530

    Added code for remote test execution

--- src/main/java/com/qa/opencart/factory/OptionsManager.java
+++ src/main/java/com/qa/opencart/factory/OptionsManager.java
@@ -19,6 +19,10 @@ public class OptionsManager {
 		co = new ChromeOptions();
 		if(Boolean.getBoolean(prop.getProperty("headless"))) co.addArguments("--headless");
 		if(Boolean.getBoolean(prop.getProperty("incognito"))) co.addArguments("--incognito");
+		
+		if(Boolean.parseBoolean(prop.getProperty("remote"))) {
+			co.setPlatformName("linux");
+		}
 		return co;
 	}
 	
@@ -26,6 +30,10 @@ public class OptionsManager {
 		fo = new FirefoxOptions();
 		if(Boolean.getBoolean(prop.getProperty("headless"))) fo.addArguments("--headless");
 		if(Boolean.getBoolean(prop.getProperty("incognito"))) fo.addArguments("--incognito");
+		if(Boolean.parseBoolean(prop.getProperty("remote"))) {
+			co.setPlatformName("linux");
+		}
+		
 		return fo;
 	}
 }
